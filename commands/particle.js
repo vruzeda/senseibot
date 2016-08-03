da3f2e6f6e205a4fec1f31cee0c2da5b1b08f2fa
bercoelho <bercoelho+git@gmail.com>
2016-08-03 17:15:10 -0300

    Added jisho particle feature

---
+++ commands/particle.js
@@ -0,0 +1,49 @@
+(function() {
+
+  var request = require('request');
+  var select = require('xpath.js');
+  var DOMParser = require('xmldom').DOMParser;
+
+  var utils = require('./utils.js');
+
+  function particle(slackRequest, slackResponse, particle) {
+	  
+    request('http://jisho.org/search/' + encodeURI(particle) + '%20%23particle', function(error, jishoResponse, jishoData) {
+      if (error) {
+        utils.postToSlack(slackResponse, 'What\'s the meaning of ' + particle + '? I don\'t know it either!');
+        return;
+      }
+  
+      var doc = new DOMParser({errorHandler: {warning: null}}).parseFromString(jishoData);
+	  
+      var wordNodes = select(doc, '//div[@class="concept_light clearfix"]');
+	 
+	  if (wordNodes.length > 0) {
+	  
+		  //first result only
+		  var meaningNodes = select(wordNodes[0], './/span[@class="meaning-meaning"]/text()');
+	
+		  if (meaningNodes.length > 0) {
+		  
+			var meaning = '';  
+			  
+			for (var i = 0; i < meaningNodes.length; ++i) {
+				if (i > 0) {
+				  meaning += '\n';
+				}
+				meaning += (i + 1) + '. ' + meaningNodes[i];
+			}
+			
+			utils.postToSlack(slackResponse, particle + ' means \n' + meaning);
+		  } else {
+			utils.postToSlack(slackResponse, 'What\'s the meaning of ' + particle + '? I don\'t know it either!');
+		  }
+	  } else {
+			utils.postToSlack(slackResponse, 'What\'s the meaning of ' + particle + '? I don\'t know it either!');
+	  }		  
+    });
+  }
+
+  module.exports = particle;
+
+}());
--- senseibot.js
+++ senseibot.js
@@ -9,6 +9,7 @@
   var kanjiReading = require('./commands/kanjiReading.js');
   var wordMeaning = require('./commands/wordMeaning.js');
   var wordReading = require('./commands/wordReading.js');
+  var particle = require('./commands/particle.js');
   var utils = require('./commands/utils.js');
 
   var app = express();
@@ -18,6 +19,7 @@
   app.use(bodyParser.text());
 
   app.post('/trigger', function (slackRequest, slackResponse) {
+	 
     if (slackRequest.body.token === variables.SLACK_TOKEN) {
       var command = slackRequest.body.text.substr(slackRequest.body.trigger_word.length).replace(/\s+/g, ' ').trim();
       var parsed = false;
@@ -26,11 +28,13 @@
       parsed = parsed || utils.parseCommand(slackRequest, slackResponse, command, /^kanji reading (.)$/, kanjiReading);
       parsed = parsed || utils.parseCommand(slackRequest, slackResponse, command, /^word meaning (.*)$/, wordMeaning);
       parsed = parsed || utils.parseCommand(slackRequest, slackResponse, command, /^word reading (.*)$/, wordReading);
+      parsed = parsed || utils.parseCommand(slackRequest, slackResponse, command, /^particle (.*)$/, particle);
 
       if (!parsed) {
         utils.postToSlack(slackResponse, '今日は、' + slackRequest.body.user_name + '！ You said: [' + command  + ']');
       }
     }
+
   });
 
   app.listen(7001, function () {
