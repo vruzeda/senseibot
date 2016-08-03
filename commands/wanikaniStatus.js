0bae295e34dae384b2a04aeae709eb7f1fa11c34
Vinicius Uzeda <vruzeda@gmail.com>
2016-08-03 19:30:33 -0300

    Minor adjustments after pull request.

--- commands/wanikaniStatus.js
+++ commands/wanikaniStatus.js
@@ -9,29 +9,29 @@
   var utils = require('./utils.js');
 
   function wanikaniStatus(slackRequest, slackResponse, user) {
-	  
-	  var apikey = variables.WANIKANI_KEYS[user];
-	  
-	  if (!apikey){
-		  utils.postToSlack(slackResponse, 'Username ' + user + ' not in the database');
-		  return;
-	  }
 
-	var action = 'study-queue';
-	  
+    var apikey = variables.WANIKANI_KEYS[user];
+
+    if (!apikey) {
+      utils.postToSlack(slackResponse, 'Username ' + user + ' not in the database');
+      return;
+    }
+
+    var action = 'study-queue';
+
     request('https://www.wanikani.com/api/user/' + apikey + '/' + action, function(error, wanikaniResponse, wanikaniData) {
       if (error) {
-        utils.postToSlack(slackResponse, 'Wanikani returned an error. '+ error);
+        utils.postToSlack(slackResponse, 'Wanikani returned an error: '+ error.message);
         return;
       }
- 
+
       var data = JSON.parse(wanikaniData);
-	  var user = data.user_information;
-	  var req = data.requested_information;
-	  
-	  var out = 'User ' + user.username + ', level '+user.level+', now has available '+req.lessons_available+' lessons and '+req.reviews_available+' reviews';
-	  
-	  utils.postToSlack(slackResponse, out);
+      var user = data.user_information;
+      var req = data.requested_information;
+
+      var out = 'User ' + user.username + ', level ' + user.level + ', now has available ' + req.lessons_available + ' lessons and ' + req.reviews_available + ' reviews';
+
+      utils.postToSlack(slackResponse, out);
 
     });
   }
--- senseibot.js
+++ senseibot.js
@@ -10,7 +10,7 @@
   var wordMeaning = require('./commands/wordMeaning.js');
   var wordReading = require('./commands/wordReading.js');
   var particle = require('./commands/particle.js');
-  var wanikaniStatus = require('./commands/wanikaniStatus.js');  
+  var wanikaniStatus = require('./commands/wanikaniStatus.js');
   var utils = require('./commands/utils.js');
 
   var app = express();
@@ -20,7 +20,6 @@
   app.use(bodyParser.text());
 
   app.post('/trigger', function (slackRequest, slackResponse) {
-	 
     if (slackRequest.body.token === variables.SLACK_TOKEN) {
       var command = slackRequest.body.text.substr(slackRequest.body.trigger_word.length).replace(/\s+/g, ' ').trim();
       var parsed = false;
@@ -36,7 +35,6 @@
         utils.postToSlack(slackResponse, '今日は、' + slackRequest.body.user_name + '！ You said: [' + command  + ']');
       }
     }
-
   });
 
   app.listen(7001, function () {
