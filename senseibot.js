79e8b820cd3b428b361631e8932785a74226c3ba
Vinicius Uzeda <vruzeda@gmail.com>
2016-09-24 10:34:32 -0300

    Implementing bot user behaviour.

--- senseibot.js
+++ senseibot.js
@@ -2,6 +2,7 @@
 
   var bodyParser = require('body-parser');
   var express = require('express');
+  var slack = require('@slack/client');
 
   var variables = require('./variables.js');
   var parseCommand = require('./commands/parseCommand.js');
@@ -12,7 +13,7 @@
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.text());
 
-  app.post('/trigger', function (slackRequest, slackResponse) {
+  app.post('/trigger', function(slackRequest, slackResponse) {
     if (slackRequest.body.token === variables.SLACK_TOKEN) {
       var userCommand = slackRequest.body.text.substr(slackRequest.body.trigger_word.length).replace(/\s+/g, ' ').trim();
       parseCommand(function(response) {
@@ -21,9 +22,27 @@
     }
   });
 
-  app.listen(7001, function () {
+  app.listen(7001, function() {
     console.log('senseibot app listening on port 7001!');
     console.log('variables: ' + JSON.stringify(variables));
   });
+
+  var rtm = new slack.RtmClient(variables.SLACK_API_TOKEN);
+
+  rtm.on(slack.CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
+    rtm.startData = rtmStartData;
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
+  });
+
+  rtm.on(slack.RTM_EVENTS.MESSAGE, function(message) {
+    if (message.type === 'message' && message.text.indexOf(`<@${rtm.startData.self.id}>`) == 0) {
+      var userCommand = message.text.substr(`<@${rtm.startData.self.id}>`.length).replace(/\s+/g, ' ').trim();
+      parseCommand(function(response) {
+        rtm.sendMessage(response, message.channel);
+      }, userCommand);
+    }
+  });
+
+  rtm.start();
 
 }());
